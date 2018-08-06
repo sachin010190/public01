10c68321f5466cc73399c61a6e725183cf6d02db
sachin010190 <42031308+sachin010190@users.noreply.github.com>
2018-08-06 22:04:09 +0530

    Update BerlinClockFixture.java

--- src/test/java/com/ubs/opsit/interviews/BerlinClockFixture.java
+++ src/test/java/com/ubs/opsit/interviews/BerlinClockFixture.java
@@ -13,24 +13,82 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class BerlinClockFixture {
 
-    private TimeConverter berlinClock;
-    private String theTime;
+       BerlinClockTest berlinClockTest = new BerlinClockTest();
+ 
+    // BLINK EVERY TWO SECONDS
+    @Test
+    public void testSS() {
+        Assert.assertEquals("Y", berlinClockTest.displaySS(0));
+        Assert.assertEquals("O", berlinClockTest.displaySS(1));
+        Assert.assertEquals("Y", berlinClockTest.displaySS(10));
+        Assert.assertEquals("O", berlinClockTest.displaySS(33));
+    }
+ 
+ 
+    // PRIMARY HOURS LAMPS
+    @Test
+    public void testPrimaryHH() {
+        Assert.assertEquals("OOOO", berlinClockTest.displayPrimaryHH(0));
+        Assert.assertEquals("OOOO", berlinClockTest.displayPrimaryHH(1));
+        Assert.assertEquals("RROO", berlinClockTest.displayPrimaryHH(12));
+        Assert.assertEquals("RRRO", berlinClockTest.displayPrimaryHH(17));
+        Assert.assertEquals("RRRR", berlinClockTest.displayPrimaryHH(24));
+    }
+ 
+    // SECONDARY HOURS LAMPS
 
     @Test
-    public void berlinClockAcceptanceTests() throws Exception {
-        aBehaviouralTestRunner()
-                .usingStepsFrom(this)
-                .withStory("berlin-clock.story")
-                .run();
+    public void testSecondaryHH() {
+        Assert.assertEquals("OOOO", berlinClockTest.displaySecondaryHH(0));
+        Assert.assertEquals("ROOO", berlinClockTest.displaySecondaryHH(1));
+        Assert.assertEquals("RROO", berlinClockTest.displaySecondaryHH(12));
+        Assert.assertEquals("RROO", berlinClockTest.displaySecondaryHH(17));
+        Assert.assertEquals("RRRO", berlinClockTest.displaySecondaryHH(23));
     }
-
-    @When("the time is $time")
-    public void whenTheTimeIs(String time) {
-        theTime = time;
+ 
+    // QUARTER TIME LAMPS
+    @Test
+    public void testQuarterLamps() {
+        String quarterTime = berlinClockTest.displayPrimaryMM(32);
+        Assert.assertEquals("R", quarterTime.substring(2, 3));
+        Assert.assertEquals("R", quarterTime.substring(5, 6));
+        Assert.assertEquals("O", quarterTime.substring(8, 9));
     }
-
-    @Then("the clock should look like $")
-    public void thenTheClockShouldLookLike(String theExpectedBerlinClockOutput) {
-        assertThat(berlinClock.convertTime(theTime)).isEqualTo(theExpectedBerlinClockOutput);
+ 
+    // PRIMARY MINUTES LAMPS
+    @Test
+    public void testPrimaryMM() {
+        Assert.assertEquals("OOOOOOOOOOO", berlinClockTest.displayPrimaryMM(0));
+        Assert.assertEquals("0OOOOOOOOOO", berlinClockTest.displayPrimaryMM(1));
+        Assert.assertEquals("YOOOOOOOOOO", berlinClockTest.displayPrimaryMM(7));
+        Assert.assertEquals("YYRYYOOOOOO", berlinClockTest.displayPrimaryMM(27));
+        Assert.assertEquals("YYRYYRYYRYY", berlinClockTest.displayPrimaryMM(59));
+    }
+ 
+    //SECONDARY MINUTES LAMPS
+    @Test
+    public void testSecondaryMM() {
+        Assert.assertEquals("OOOO", berlinClockTest.displaySecondaryMM(0));
+        Assert.assertEquals("YOOO", berlinClockTest.displaySecondaryMM(1));
+        Assert.assertEquals("YYOO", berlinClockTest.displaySecondaryMM(7));
+        Assert.assertEquals("YYOO", berlinClockTest.displaySecondaryMM(27));
+        Assert.assertEquals("YYYY", berlinClockTest.displaySecondaryMM(59));
+    }
+ 
+    // BERLIN CLOCK INPUT
+    @Test
+    public void testBerlinClockInput()  {
+        Assert.assertEquals(5, berlinClockTest.getBerlinTime("23:33:33").length);
+    }
+ 
+    // BERLIN CLOCK RESULTS TEST
+    @Test
+    public void testBerlinClockOutput() {
+        String[] berlinTime = berlinClockTest.convertTime("16:37:16");
+        String[] expectedResult = new String[] {"O", "RRRR", "RRRO", "YYRYYROOOOO", "YYYO"};
+        Assert.assertEquals(expectedResult.length, berlinTime.length);
+        for (int index = 0; index < expectedResult.length; index++) {
+            Assert.assertEquals(expectedResult[index], berlinTime[index]);
+        }
     }
 }
