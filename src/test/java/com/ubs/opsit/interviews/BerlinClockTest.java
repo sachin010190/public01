229a47feb49d280767dbed89191d20db32273f6f
sachin010190 <42031308+sachin010190@users.noreply.github.com>
2018-08-06 10:59:06 +0530

    Create BerlinClockTest.java

---
+++ src/test/java/com/ubs/opsit/interviews/BerlinClockTest.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BerlinClockTest {
+ 
+    public String[] getBerlinTime(String time) {
+    	List<Integer> parts = new ArrayList<Integer>();
+    	for (String part : time.split(":")) {
+    	  parts.add(Integer.parseInt(part));
+    	}
+    	
+        return new String[] {
+                getSeconds(parts.get(2)),
+                getTopHours(parts.get(0)),
+                getBottomHours(parts.get(0)),
+                getTopMinutes(parts.get(1)),
+                getBottomMinutes(parts.get(1))
+        };
+    }
+ 
+    protected String getSeconds(int number) {
+        if (number % 2 == 0) return "Y";
+        else return "O";
+    }
+ 
+    protected String getTopHours(int number) {
+        return getOnOff(4, getTopNumberOfOnSigns(number));
+    }
+ 
+    protected String getBottomHours(int number) {
+        return getOnOff(4, number % 5);
+    }
+ 
+    protected String getTopMinutes(int number) {
+        return getOnOff(11, getTopNumberOfOnSigns(number), "Y").replaceAll("YYY", "YYR");
+    }
+ 
+    protected String getBottomMinutes(int number) {
+        return getOnOff(4, number % 5, "Y");
+    }
+ 
+    private String getOnOff(int lamps, int onSigns) {
+        return getOnOff(lamps, onSigns, "R");
+    }
+    private String getOnOff(int lamps, int onSigns, String onSign) {
+        String out = "";
+        for (int i = 0; i < onSigns; i++) {
+            out += onSign;
+        }
+        for (int i = 0; i < (lamps - onSigns); i++) {
+            out += "O";
+        }
+        return out;
+    }
+ 
+    private int getTopNumberOfOnSigns(int number) {
+        return (number - (number % 5)) / 5;
+    } 
+}
